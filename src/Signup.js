7f91822a5277a5bade18ad4d7fe2ca77b589e18f
ragss8 <96374035+ragss8@users.noreply.github.com>
2023-06-16 16:10:12 +0530

    Update Signup.js

--- src/Signup.js
+++ src/Signup.js
@@ -1,21 +1,22 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import './Signup.css'
+import './Signup.css';
 
 const SignUp = () => {
-  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   const handleSignUp = async (e) => {
     e.preventDefault();
     try {
       // Send sign-up request to the server or API using Axios
-      const response = await axios.post('/api/signup', { username, password });
+      const response = await axios.post('http://localhost:8000/users', { email, password });
 
       // Check the response status
       if (response.status === 200) {
         // Sign-up successful
         alert('Sign up successful!');
+        window.location.href = '/Login'; // Redirect to the login page
       } else {
         // Sign-up failed
         alert('Sign up failed!');
@@ -28,23 +29,23 @@ const SignUp = () => {
   return (
     <body>
       <div className="container">
-      <h2>Sign Up</h2>
-      <form onSubmit={handleSignUp}>
-        <input
-          type="text"
-          placeholder="Username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit">Sign Up</button>
-      </form>
-    </div>
+        <h2>Sign Up</h2>
+        <form onSubmit={handleSignUp}>
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit">Sign Up</button>
+        </form>
+      </div>
     </body>
   );
 };
