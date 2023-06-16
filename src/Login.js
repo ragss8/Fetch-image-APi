f34f65cc10c374680b6b23fca712c44b7b594fd2
ragss8 <96374035+ragss8@users.noreply.github.com>
2023-06-16 16:09:20 +0530

    Update Login.js

--- src/Login.js
+++ src/Login.js
@@ -1,36 +1,69 @@
-
 import React, { useState } from 'react';
+import axios from 'axios';
+import App from './App';
+import './Login.css'
 
 const Login = () => {
-  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [route, setRoute] = useState('login');
+  const [sessionToken, setSessionToken] = useState('');
+  const [loggedIn, setLoggedIn] = useState(false);
 
-  const handleLogin = (e) => {
+  const handleLogin = async (e) => {
     e.preventDefault();
-    // Handle login logic here
-    console.log('Login:', { username, password });
+    try {
+      const response = await axios.post('http://localhost:8000/login', {
+        email,
+        password,
+      });
+      if (response.status === 200) {
+        const receivedSessionToken = response.data.session_token;
+        setSessionToken(receivedSessionToken);
+        setLoggedIn(true);
+      } else {
+        console.log('Login failed:', response.data.message);
+      }
+      setRoute('App');
+    } catch (error) {
+      console.error('Login error:', error);
+    }
   };
 
-  return (
-    <div className="container">
-      <h2>Login</h2>
-      <form onSubmit={handleLogin}>
-        <input
-          type="text"
-          placeholder="Username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit">Login</button>
-      </form>
-    </div>
-  );
+  const renderComponent = () => {
+    switch (route) {
+      case 'login':
+        return (
+          <div className="container login"> {/* Added "login" class name */}
+            <h2>Login</h2>
+            <form onSubmit={handleLogin}>
+              <input
+                type="text"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <button type="submit">Login</button>
+            </form>
+            <p>
+              <h4>IF A NEW USER, PLEASE SIGNUP</h4>
+            </p>
+          </div>
+        );
+      case 'App':
+        return <App />;
+      default:
+        return null;
+    }
+  };
+
+  return renderComponent();
 };
 
 export default Login;
