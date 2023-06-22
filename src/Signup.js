84259a5264e54bc4aca859380fb553f476cf64fb
ragss8 <96374035+ragss8@users.noreply.github.com>
2023-06-22 09:43:19 +0530

    Update Signup.js

--- src/Signup.js
+++ src/Signup.js
@@ -2,51 +2,52 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import './Signup.css';
 
-const SignUp = () => {
+const SignUp = ({ setRoute }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   const handleSignUp = async (e) => {
     e.preventDefault();
     try {
-      // Send sign-up request to the server or API using Axios
       const response = await axios.post('http://localhost:8000/users', { email, password });
-
-      // Check the response status
       if (response.status === 200) {
-        // Sign-up successful
-        alert('Sign up successful!');
-        window.location.href = '/Login'; // Redirect to the login page
+        alert('Sign up successful! Please click on Login!!');
+        setRoute('login'); // Set the route to 'login' after successful signup
       } else {
-        // Sign-up failed
         alert('Sign up failed!');
       }
     } catch (error) {
-      console.error('Error signing up:', error);
+      alert('Error signing up:'+ error.message);
+    }
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      handleSignUp(e);
     }
   };
 
   return (
-    <body>
-      <div className="container">
-        <h2>Sign Up</h2>
-        <form onSubmit={handleSignUp}>
-          <input
-            type="email"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <button type="submit">Sign Up</button>
-        </form>
-      </div>
-    </body>
+    <div className="container">
+      <h2>Sign Up</h2>
+      <form onSubmit={handleSignUp}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          onKeyPress={handleKeyPress} // Add onKeyPress event listener
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          onKeyPress={handleKeyPress} // Add onKeyPress event listener
+        />
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
   );
 };
 
